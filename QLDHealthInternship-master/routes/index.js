8f4e62ceda1a604e0bcbb251e56de963b748bd8b
Trent Marino <trent.marino@my.jcu.edu.au>
2015-12-09 19:58:44 +1000

    sure mate

---
+++ QLDHealthInternship-master/routes/index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var formidable = require('formidable');
+var DAO = require('../DAO');
+var noticeHandler = require('../control/noticeHandler.js');
+var gridder = require('../DAO/gridder');
+
+
+router.get('/', function(req, res, next) {
+    res.render('index');
+});
+router.get('/training', function(req, res, next) {
+    DAO.pullTable(function(myDocs){
+        console.log(JSON.stringify(myDocs[0]));
+        res.render('training', {myArray : myDocs});
+    });});
+
+
+router.get('/notice', function(req, res, next) {
+    DAO.pullTable(function(myDocs){
+        console.log(JSON.stringify(myDocs[0]));
+        res.render('notice', {myArray : myDocs});
+    });
+});
+
+
+
+
+router.get('/upload', function(req, res, next) {
+    res.render('upload');
+});
+
+router.post('/upload', function(req, res){
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields){
+    if(err){
+      throw err;
+    }
+      noticeHandler.handleAddNotice(fields);
+      res.redirect('/');
+  })
+});
+
+
+
+module.exports = router;
